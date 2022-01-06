8029b6a889052e40d4f87c49f8494bb3646583b8
Anthony Todesco <adtodesco@gmail.com>
2022-01-06 15:34:05 -0600

    Initial commit with the Flask templated generated by cookiecutter-flask

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: flask db upgrade
+web: gunicorn sportblurbsserver.app:create_app\(\) -b 0.0.0.0:$PORT -w 3
---
+++ autoapp.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Create an application instance."""
+from sportblurbsserver.app import create_app
+
+app = create_app()
