29951dd8fad0c8f2f255b92ea6de6f8f5e3f775b
Anthony Todesco <adtodesco@gmail.com>
2022-02-15 22:50:51 -0800

    Adds infinite-scroll and autohide nav bar
    * Adds infinite-scroll to home page that uses javascript to call the 'load'
      API that returns additional blurbs when the user reaches the bottom of the
      page.
    * Autohides nav bar on scroll down and reappears on scroll up.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,94 @@
-// App initialization code goes here
+// Logic for infinite loading
+
+// Get references to the dom elements
+var scroller = document.querySelector('#scroller');
+var template = document.querySelector('#post_template');
+var sentinel = document.querySelector('#sentinel');
+
+// Set a counter to count the items loaded
+var counter = 0;
+
+// Function to request new items and render to the dom
+function loadItems() {
+
+  // Use fetch to request data and pass the counter value in the QS
+  fetch(`/load?c=${counter}`).then((response) => {
+
+    // Convert the response data to JSON
+    response.json().then((data) => {
+
+      // If empty JSON, exit the function
+      if (!data.length) {
+
+        // Replace the spinner with "No more posts"
+        sentinel.innerHTML = 'No more posts';
+        return;
+      }
+
+      // Iterate over the items in the response
+      for (var i = 0; i < data.length; i++) {
+
+        // Clone the HTML template
+        let template_clone = template.content.cloneNode(true);
+
+        const date = new Date(data[i]['date']);
+        var date_options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+        // Query & update the template content
+        let title_data = [data[i]['name'], data[i]['position'], data[i]['team']]
+        template_clone.querySelector('#title').innerHTML = title_data.join(' | ')
+        template_clone.querySelector('#date').innerHTML = date.toLocaleDateString('en-US', date_options);
+        template_clone.querySelector('#news').innerHTML = data[i]['news'];
+        template_clone.querySelector('#spin').innerHTML = data[i]['spin'];
+
+        // Append template to dom
+        scroller.appendChild(template_clone);
+
+        // Increment the counter
+        counter += 1;
+      }
+    })
+  })
+}
+
+// Create a new IntersectionObserver instance
+var intersectionObserver = new IntersectionObserver(entries => {
+  // If intersectionRatio is 0, the sentinel is out of view
+  // and we don't need to do anything. Exit the function
+  if (entries[0].intersectionRatio <= 0) {
+    return;
+  }
+
+  // Call the loadItems function
+  loadItems();
+
+});
+
+// Instruct the IntersectionObserver to watch the sentinel
+intersectionObserver.observe(sentinel);
+
+
+// Logic for showing/hiding filter nav bar
+
+document.addEventListener('DOMContentLoaded', function() {
+  el_autohide = document.querySelector('.autohide');
+  // add padding-top to body (if necessary)
+  navbar_height = document.querySelector('.navbar').offsetHeight;
+  document.body.style.paddingTop = navbar_height + 'px';
+
+  if(el_autohide){
+    var last_scroll_top = 0;
+    window.addEventListener('scroll', function() {
+         let scroll_top = window.scrollY;
+         if (scroll_top < last_scroll_top) {
+              el_autohide.classList.remove('scrolled-down');
+              el_autohide.classList.add('scrolled-up');
+          }
+          else {
+              el_autohide.classList.remove('scrolled-up');
+              el_autohide.classList.add('scrolled-down');
+          }
+          last_scroll_top = scroll_top;
+    });
+  }
+});
--- sportblurbsserver/public/views.py
+++ sportblurbsserver/public/views.py
@@ -1,20 +1,50 @@
 # -*- coding: utf-8 -*-
 """Public section, including homepage and signup."""
-from flask import (
-    Blueprint,
-    current_app,
-    render_template,
-)
+from flask import Blueprint, current_app, jsonify, make_response, redirect, render_template, request, url_for
+from sportblurbs.database import get_documents, BLURB_COLLECTION
+
+from sportblurbsserver.extensions import mongo
 
 blueprint = Blueprint("public", __name__, static_folder="../static")
 
 
-# @blueprint.route("/", methods=["GET", "POST"])
 @blueprint.route("/")
 def home():
     """Home page."""
-    current_app.logger.info("Hello from the home page!")
     return render_template("public/home.html")
+
+
+posts_per_load = 20  # num posts to return per request
+
+
+@blueprint.route("/load")
+def load():
+    """ Route to return the posts """
+    if request.args:
+        counter = int(request.args.get("c"))  # The 'counter' value sent in the QS
+        league = request.args.get("l")
+
+        current_app.logger.info(f"Returning posts {counter} to {counter + posts_per_load}")
+        # Slice counter -> quantity from the db
+        blurbs = [
+            {
+                "name": b["player"]["name"],
+                "position": b["player"]["position"],
+                "team": b["player"]["team"],
+                "date": b["date"],
+                "news": b["blurb"]["news"],
+                "spin": b["blurb"]["spin"],
+            }
+            for b in get_documents(mongo.db, BLURB_COLLECTION)[counter: counter + posts_per_load]
+        ]
+        if len(blurbs) == 0:
+            current_app.logger.info("No more blurbs")
+            return make_response(jsonify({}), 200)
+        else:
+            current_app.logger.info(f"Returning {len(blurbs)} blurbs")
+            return make_response(jsonify(blurbs), 200)
+
+    return redirect(url_for("public.home"))
 
 
 @blueprint.route("/about/")
